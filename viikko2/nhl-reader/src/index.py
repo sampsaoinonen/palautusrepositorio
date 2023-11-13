344b7e907c2c95d764647e8305b546e6f3340b68
sampsaoinonen <sampsa.oinonen@gmail.com>
2023-11-13 23:47:28 +0200

    Add assignment 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,39 +1,47 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.players = []
+        self.get_json_data()
+
+    def get_json_data(self):
+        self.response = requests.get(self.url).json()
+
+    def get_players(self):
+        return [Player(
+            player['name'],
+            player['team'],
+            player['nationality'],
+            player['games'],
+            player['goals'],
+            player['assists']
+        ) for player in self.response]
+
+class PlayerStats:
+    def __init__(self, players):
+        self.players = players
+
+    def top_scorers_by_nationality(self, nationality):
+        finnish_players = filter(lambda x: x.nationality == nationality, self.players)
+        return sorted(
+            finnish_players,
+            key=lambda x: x.goals + x.assists,
+            reverse=True
+        )
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    players = reader.get_players()
 
-    players = []
+    stats = PlayerStats(players)
+    finnish_players = stats.top_scorers_by_nationality("FIN")
 
-    for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['team'],
-            player_dict['nationality'],
-            player_dict['games'],
-            player_dict['goals'],
-            player_dict['assists']
-        )
-        players.append(player)
-    
-    print("Players from FIN\n")
-
-    finnish_players = [player for player in players if player.nationality == "FIN"]
-
-    sort_finnish_players = sorted(
-        finnish_players,
-        key=lambda i: i.goals + i.assists,
-        reverse=True
-    )
-
-    for player in sort_finnish_players:
-        print(
-            f"{player.name:20} {player.team} {player.goals} + {player.assists} = {player.goals + player.assists}"
-        )
-
-
+    for player in finnish_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
